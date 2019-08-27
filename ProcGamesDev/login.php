4922bf9a3458cd31c3ad865e583933090b2f8cb7
prcordova <43557558+prcordova@users.noreply.github.com>
2019-08-26 22:37:11 -0300

    Add files via upload

---
+++ ProcGamesDev/login.php
@@ -0,0 +1,43 @@
+<html>
+
+    <head>
+            <title>Formulario</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    </head>
+    <body>
+            <div id="conteudo"> 
+
+              
+       
+<?php
+
+            <form action="recurso no servidor">
+                <table border="10">
+                    <tr>
+                        <td>Nome:</td>
+                        <td><input type="text" name="nome"></td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td><input type="text" name="email"></td>
+                    </tr>
+                    <tr>
+                        <td>Sexo:</td>
+                        <td><select name="sexo">
+                            <option value="m">Masculino</option>
+                            <option value="f">Feminino</option>
+                         
+                        </select></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="submit" value="Cadastrar"></td>
+                     
+                    </tr>
+
+                </table>
+            </form>
+        </div> ?>
+        
+    </body>
+</html>
